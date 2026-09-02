5baa498b2f0c3c426cbb894371e180d8af89e36d
Zeyad Ammar <zeyadammar2510@gmail.com>
2026-09-03 00:32:01 +0300

    end the project w/ all lights events

---
+++ TrafficLights/ctrlTraficLight.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TrafficLights
+{
+    partial class ctrlTraficLight
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbTrafficLight = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbTimer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTrafficLight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbTrafficLight
+            // 
+            this.pbTrafficLight.Image = global::TrafficLights.Properties.Resources.AllLights;
+            this.pbTrafficLight.Location = new System.Drawing.Point(0, 0);
+            this.pbTrafficLight.Name = "pbTrafficLight";
+            this.pbTrafficLight.Size = new System.Drawing.Size(190, 263);
+            this.pbTrafficLight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTrafficLight.TabIndex = 0;
+            this.pbTrafficLight.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbTimer
+            // 
+            this.lbTimer.AutoSize = true;
+            this.lbTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimer.Location = new System.Drawing.Point(77, 277);
+            this.lbTimer.Name = "lbTimer";
+            this.lbTimer.Size = new System.Drawing.Size(36, 25);
+            this.lbTimer.TabIndex = 1;
+            this.lbTimer.Text = "??";
+            // 
+            // ctrlTraficLight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbTimer);
+            this.Controls.Add(this.pbTrafficLight);
+            this.Name = "ctrlTraficLight";
+            this.Size = new System.Drawing.Size(190, 317);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTrafficLight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbTrafficLight;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbTimer;
+    }
+}
---
+++ TrafficLights/ctrlTraficLight.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TrafficLights.Properties;
+
+namespace TrafficLights
+{
+    public partial class ctrlTraficLight : UserControl
+    {
+
+        public enum enColors { RED, YELLOW , GREEN };
+
+        private enColors _currentColor;
+        public enColors CurrentColor { get { return _currentColor; }
+            
+            set {
+
+                if (value != _currentColor)
+                {
+                    _currentColor = value;
+
+                    if (_currentColor == enColors.RED)
+                    {
+                        pbTrafficLight.Image = Resources.red;
+                        lbTimer.ForeColor = Color.Red;
+
+                    }
+                    else if(_currentColor == enColors.YELLOW)
+                    {
+                        pbTrafficLight.Image = Resources.yello;
+                        lbTimer.ForeColor = Color.Yellow;
+                    }
+                    else
+                    {
+                        pbTrafficLight.Image = Resources.Green;
+                        lbTimer.ForeColor = Color.Green;
+                    }
+                }
+
+            }
+        }
+
+        
+
+
+
+        public enColors PreviousColor;
+
+        public int RedTimer { get; set; }
+
+        public int GreenTimer { get; set; }
+
+
+        public int YellowTimer { get; set; }
+
+
+        public int Timer;
+
+        public ctrlTraficLight()
+        {
+
+            InitializeComponent();
+            
+        }
+        
+        public class CustomEventArgs : EventArgs
+        {
+            public int Timer { get; set; }
+            public enColors Color { get; set; }
+            
+
+            public CustomEventArgs(int Timer,enColors Colors)
+            {
+                this.Timer= Timer;
+                this.Color = Colors;
+            }
+        }
+
+        public event EventHandler<CustomEventArgs> onRedLight;
+
+        private void RaseRedLightEvent()
+        {
+            RaseRedLightEvent(new CustomEventArgs ( RedTimer,enColors.RED));
+        }
+        private void RaseRedLightEvent(CustomEventArgs e)
+        {
+            onRedLight?.Invoke(this, e);
+        }
+
+
+        public event EventHandler<CustomEventArgs> onGreenLight;
+
+        private void RaseGreenLightEvent()
+        {
+            RaseGreenLightEvent(new CustomEventArgs(GreenTimer, enColors.GREEN));
+        }
+        private void RaseGreenLightEvent(CustomEventArgs e)
+        {
+            onRedLight?.Invoke(this, e);
+        }
+
+        public event EventHandler<CustomEventArgs> onYellowLight;
+
+        private void RaseYellowLightEvent()
+        {
+            RaseYellowLightEvent(new CustomEventArgs(YellowTimer, enColors.YELLOW));
+        }
+        private void RaseYellowLightEvent(CustomEventArgs e)
+        {
+            onRedLight?.Invoke(this, e);
+        }
+        public void ctrlStart()
+        {
+            
+            switch (CurrentColor)
+            {
+                case enColors.RED:
+                    ChangeColorToRed();
+                    break;
+                case enColors.YELLOW:
+                    ChangeColorToYellow();
+                    break;
+                case enColors.GREEN:
+                    ChangeColorToGreen();
+                    break;
+
+                default:
+                    ChangeColorToRed();
+                    break;
+            }
+            
+            timer1.Start();
+            
+        }
+
+        
+        private void ChangeColorToRed()
+        {
+            PreviousColor = CurrentColor;
+            CurrentColor = enColors.RED;
+            pbTrafficLight.Image = Resources.red;
+            lbTimer.ForeColor= Color.Red;
+            Timer = RedTimer;
+            lbTimer.Text = Timer.ToString();
+
+            RaseRedLightEvent();
+        }
+
+
+        private void ChangeColorToGreen()
+        {
+            PreviousColor = CurrentColor;
+            CurrentColor = enColors.GREEN;
+            pbTrafficLight.Image = Resources.Green;
+            lbTimer.ForeColor = Color.Green;
+            Timer = GreenTimer;
+            lbTimer.Text = Timer.ToString();
+
+            RaseGreenLightEvent();
+
+        }
+
+        private void ChangeColorToYellow()
+        {
+            PreviousColor = CurrentColor;
+            CurrentColor= enColors.YELLOW;
+            pbTrafficLight.Image = Resources.yello;
+            lbTimer.ForeColor = Color.Yellow;
+            Timer = YellowTimer;
+            lbTimer.Text = Timer.ToString();
+
+            RaseYellowLightEvent();
+
+        }
+
+
+        public void LightChange(enColors Color)
+        {
+            switch (Color)
+            {
+                case enColors.RED:
+                    ChangeColorToRed();
+                    break;
+                case enColors.GREEN:   
+                    ChangeColorToGreen();
+                    break;
+                case enColors.YELLOW:
+                    ChangeColorToYellow();
+                    break;
+
+                default:
+                    ChangeColorToRed();
+                    break;
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Timer--;
+            lbTimer.Text=Timer.ToString();
+
+            if (Timer == 0)
+            {
+
+                if (CurrentColor == enColors.RED||CurrentColor==enColors.GREEN)
+                {
+                    ChangeColorToYellow();
+
+                }else if (CurrentColor == enColors.YELLOW)
+                {
+                    if (PreviousColor == enColors.RED)
+                    {
+                        ChangeColorToGreen();
+                    }
+                    else
+                    {
+                        ChangeColorToRed();
+                    }
+                }
+
+
+                
+            }
+        }
+
+        
+    }
+}
